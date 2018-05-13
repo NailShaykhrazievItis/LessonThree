a17c9ef8ea19818d34df4d6b40e976f9f6b04f2c
Almaz <Alzarg@yandex.ru>
2018-05-13 16:55:05 +0300

    NavigationDrawer + popular movies activity

---
+++ app/src/main/java/com/itis/android/lessonmvvm/ui/BaseActivity.kt
@@ -0,0 +1,81 @@
+package com.itis.android.lessonmvvm.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import com.itis.android.lessonmvvm.R
+import kotlinx.android.synthetic.main.activity_base.*
+
+
+open class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+
+
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    protected fun supportActionBar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        initNavigationDrawer(toolbar)
+    }
+
+    protected fun setBackArrow(toolbar: Toolbar) {
+        val actionBar = supportActionBar
+        actionBar?.let {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setDisplayShowHomeEnabled(true)
+            toolbar.setNavigationOnClickListener { v -> onBackPressed() }
+        }
+    }
+
+    private fun initNavigationDrawer(toolbar: Toolbar) {
+        nav_view.setNavigationItemSelectedListener({ menuItem ->
+            val id = menuItem.getItemId()
+            when (id) {
+                R.id.menu_top_rated -> {
+                    val intent = Intent(applicationContext, TopRatedMoviesListActivity::class.java)
+                    startActivity(intent)
+                    menuItem.isChecked = true
+                    drawer_layout.closeDrawers()
+                }
+                R.id.menu_popular -> {
+                    val intent = Intent(applicationContext, PopularMoviesListActivity::class.java)
+                    startActivity(intent)
+                    menuItem.isChecked = true
+                    drawer_layout.closeDrawers()
+                }
+            }
+            true
+        })
+//        /*
+//          Если хотите получить элементы из верхней части бокового меню, то это делается так.
+//          Example:
+//          View header = mNavigationView.getHeaderView(0);
+//          TextView menuText = header.findViewById(R.id.tv_menu);
+//         */
+//        val header = nav_view.getHeaderView(0)
+//        val menuCover = header.findViewById(R.id.iv_cover)
+//        ImageLoadHelper.loadPictureByDrawable(menuCover, R.drawable.image_marvel_logo)
+        setActionBar(toolbar)
+    }
+
+    private fun setActionBar(toolbar: Toolbar) {
+        val actionBarDrawerToggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
+                R.string.drawer_open, R.string.drawer_close)
+        drawer_layout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+}
--- app/src/main/java/com/itis/android/lessonmvvm/ui/movielist/MovieListActivity.kt
+++ app/src/main/java/com/itis/android/lessonmvvm/ui/movielist/MovieListActivity.kt
@@ -1,43 +1,47 @@
 package com.itis.android.lessonmvvm.ui.movielist
 
 import android.app.ActivityOptions
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.itis.android.lessonmvvm.R
 import com.itis.android.lessonmvvm.di.di
+import com.itis.android.lessonmvvm.model.Response
+import com.itis.android.lessonmvvm.model.api_response.movie.Movie
+import com.itis.android.lessonmvvm.ui.BaseActivity
 import com.itis.android.lessonmvvm.ui.MovieDetailsActivity
 import com.itis.android.lessonmvvm.ui.ViewModelFactory
 import com.itis.android.lessonmvvm.utils.ARG_MOVIE
+import kotlinx.android.synthetic.main.activity_base.*
 import kotlinx.android.synthetic.main.activity_movie_list.*
 import kotlinx.android.synthetic.main.layout_recycler_view.*
 import org.kodein.di.generic.instance
 
-class MovieListActivity : AppCompatActivity() {
+abstract class MovieListActivity : BaseActivity() {
 
     private val viewModelFactory: ViewModelFactory by di.instance()
-    private lateinit var viewModel: MovieListViewModel
+    protected lateinit var viewModel: MovieListViewModel
     private var adapter: MovieListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_list)
+        layoutInflater.inflate(R.layout.activity_movie_list, drawer_container)
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(MovieListViewModel::class.java)
         initRecycler()
-        setSupportActionBar(tb_movie_list)
+        supportActionBar(tb_movie_list)
         observeMovieList()
         observeProgressBar()
         observeItemClick()
     }
 
     private fun observeMovieList() =
-            viewModel.getTopRatedMoviesList()?.observe(this, Observer {
+            getMoviesList()?.observe(this, Observer {
                 when {
                     it?.data != null -> {
                         adapter?.updateData(it.data)
@@ -51,6 +55,8 @@ class MovieListActivity : AppCompatActivity() {
                     }
                 }
             })
+
+    protected abstract fun getMoviesList(): LiveData<Response<List<Movie>>>?
 
     private fun observeProgressBar() =
             viewModel.isLoading().observe(this, Observer {
